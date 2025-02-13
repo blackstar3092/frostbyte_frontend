926934b0bf88ecaa95666ffe5a06524077b09d77
93551990 <blackstar3092@gmail.com>
2025-02-12 18:24:17 -0800

    fix for deployment

--- assets/js/api/config.js
+++ assets/js/api/config.js
@@ -4,7 +4,7 @@ if (location.hostname === "localhost") {
 } else if (location.hostname === "127.0.0.1") {
         pythonURI = "http://127.0.0.1:8102";
 } else {
-        pythonURI =  "https://flask2025.nighthawkcodingsociety.com";
+        pythonURI =  "https://frostbyte.stu.nighthawkcodingsociety.com";
 }
 export var javaURI;
 if (location.hostname === "localhost") {
